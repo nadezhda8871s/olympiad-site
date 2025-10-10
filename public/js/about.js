43bc54de0d73271a3fdc54e87351ae72411323ed
nadezhda8871s <nadezhda8871s@gmail.com>
2025-10-10 23:03:30 +0300

    Update about.js

--- public/js/about.js
+++ public/js/about.js
@@ -20,7 +20,14 @@ document.addEventListener('DOMContentLoaded', () => {
             if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
             const aboutData = await response.json();
 
-           
+            // Заполняем статические поля
+            innSpan.textContent = aboutData.inn || '231120569701';
+            phoneSpan.textContent = aboutData.phone || '89184455287';
+            addressSpan.textContent = aboutData.address || 'г. Краснодар, ул. Виноградная, 58';
+            emailLink.href = `mailto:${aboutData.email || 'vsemnayka@gmail.com'}`;
+            emailLink.textContent = aboutData.email || 'vsemnayka@gmail.com';
+            requisitesPre.textContent = aboutData.requisites || 'ООО "РУБИКОН-ПРИНТ"\nИНН: 2311372333\nР/с: 40702810620000167717\nБанк: ООО "Банк Точка"\nБИК: 044525104\nК/с: 30101810745374525104';
+
             // Заполняем редактируемое поле
             // Предположим, что в `aboutData` есть поле `customText`
             editableTextParagraph.textContent = aboutData.customText || 'Добро пожаловать! Участвуйте в олимпиадах, конкурсах научных работ, ВКР и конференциях!\nДокументы формируются в течение 5 дней. Удобная оплата. Высокий уровень мероприятий.';
