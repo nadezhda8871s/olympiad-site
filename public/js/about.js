17a804a12327757633a5ed1e1a2c2d87d9a74c67
nadezhda8871s <nadezhda8871s@gmail.com>
2025-10-11 10:16:23 +0300

    Delete public directory

--- public/js/about.js
+++
@@ -1,8 +0,0 @@
-/* js/about.js */
-(function () {
-  const el = document.getElementById('about-content');
-  fetch('/api/about')
-    .then(r => r.json())
-    .then(({ customText }) => el && (el.textContent = customText || 'Пока текст не добавлен.'))
-    .catch(() => el && (el.textContent = 'Не удалось загрузить текст.'));
-})();
