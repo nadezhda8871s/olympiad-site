f309a1eeecd8885c05072bdc86a61709a1f4e27a
nadezhda8871s <nadezhda8871s@gmail.com>
2025-10-12 23:01:04 +0300

    Add files via upload

--- config/urls.py
+++ config/urls.py
@@ -1,22 +1,35 @@
+
 from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import path, include
-from django.conf.urls.i18n import set_language
+# Важно: правильный импорт set_language (иначе ImportError и 502)
+from django.views.i18n import set_language
 
 def healthz(_request):
     return HttpResponse("ok", content_type="text/plain")
 
 def favicon(_request):
-    # Пустая "иконка", чтобы браузеры не сыпали 404
+    # Пустая иконка, чтобы браузеры не сыпали 404
     return HttpResponse(b"", content_type="image/x-icon", status=200)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("healthz", healthz),
     path("favicon.ico", favicon),
-    # i18n: для {% url 'set_language' %} из base.html
+    # для {% url 'set_language' %} из шаблонов
     path("i18n/setlang/", set_language, name="set_language"),
-    # Основные приложения
-    path("", include("pages.urls")),   # главная и "О нас"
-    path("", include("events.urls")),  # разделы мероприятий
 ]
+
+# Подключаем приложения. Если модуль недоступен, не валим весь сайт.
+try:
+    urlpatterns += [path("", include("pages.urls"))]
+except Exception:
+    def _home(_request):
+        return HttpResponse("home ok", content_type="text/plain")
+    urlpatterns.append(path("", _home))
+
+try:
+    urlpatterns += [path("", include("events.urls"))]
+except Exception:
+    # просто пропускаем events, чтобы сайт не падал при импорте
+    pass
