081438b71be31501434ce0c7a899553b4ffdb72a
nadezhda8871s <nadezhda8871s@gmail.com>
2025-10-12 16:02:44 +0300

    Add files via upload

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,2 +1,14 @@
+
 from .base import *  # noqa
+
 DEBUG = False
+
+ALLOWED_HOSTS = [".onrender.com", "localhost", "127.0.0.1"]
+CSRF_TRUSTED_ORIGINS = ["https://*.onrender.com"]
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+SECURE_SSL_REDIRECT = True
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
