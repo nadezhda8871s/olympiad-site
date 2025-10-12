c0481bce0e2b60f13bba3b6f0761d0aba8349043
nadezhda8871s <nadezhda8871s@gmail.com>
2025-10-12 22:18:13 +0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 def main():
-    # Если на Render задан DJANGO_SETTINGS_MODULE — он приоритетнее
+    # На Render желательно использовать prod-настройки. Если в окружении задано иначе — они приоритетнее.
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.prod")
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
