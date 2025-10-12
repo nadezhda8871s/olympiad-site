ff14821f0e196f1b83f40e6ecc78d617a5036bd3
nadezhda8871s <nadezhda8871s@gmail.com>
2025-10-12 14:38:49 +0300

    Update init_admin.py

--- events/management/commands/init_admin.py
+++ events/management/commands/init_admin.py
@@ -4,16 +4,13 @@ from django.contrib.auth import get_user_model
 class Command(BaseCommand):
     help = "Создаёт/обновляет администратора: nadezhda8871s / 1988naNA"
 
-    def handle(self, *args, **options):
+    def handle(self, *args, **kwargs):
         User = get_user_model()
         username = "nadezhda8871s"
         password = "1988naNA"
         user, created = User.objects.get_or_create(username=username, defaults={"is_staff": True, "is_superuser": True})
-        if created:
-            self.stdout.write(self.style.SUCCESS("Создан админ-пользователь."))
-        # Всегда обновляем права и пароль
         user.is_staff = True
         user.is_superuser = True
         user.set_password(password)
         user.save()
-        self.stdout.write(self.style.SUCCESS("Админ-пользователь готов: nadezhda8871s / 1988naNA"))
+        self.stdout.write(self.style.SUCCESS("OK: admin nadezhda8871s / 1988naNA готов"))
