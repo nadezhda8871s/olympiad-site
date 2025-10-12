27a457dd0858946e433305f905c1b0603099758d
nadezhda8871s <nadezhda8871s@gmail.com>
2025-10-12 22:42:01 +0300

    Add files via upload

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import path, include
+from django.conf.urls.i18n import set_language
 
 def healthz(_request):
     return HttpResponse("ok", content_type="text/plain")
@@ -13,6 +14,9 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("healthz", healthz),
     path("favicon.ico", favicon),
+    # i18n: для {% url 'set_language' %} из base.html
+    path("i18n/setlang/", set_language, name="set_language"),
+    # Основные приложения
     path("", include("pages.urls")),   # главная и "О нас"
     path("", include("events.urls")),  # разделы мероприятий
 ]
