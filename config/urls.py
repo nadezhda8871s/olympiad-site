fb465b7748ba40eced9541a781489edc93e4e169
nadezhda8871s <nadezhda8871s@gmail.com>
2025-10-12 12:55:41 +0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("events.urls")),
-    path("pages/", include("pages.urls")),
+    path("", include("pages.urls")),            # /about/
+    path("i18n/", include("django.conf.urls.i18n")),  # /i18n/setlang/
 ]
+
+# ВНИМАНИЕ: Для небольших проектов можно отдать media прямо Django.
+# На Render это допустимо. Для высокой нагрузки лучше CDN/S3.
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
