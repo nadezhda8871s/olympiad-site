021ef30ba04c5f65f64e59dd3753a27e0f39cbca
nadezhda8871s <nadezhda8871s@gmail.com>
2025-10-12 20:10:45 +0300

    Update urls.py

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
+from django.http import HttpResponse
+
+def healthz(request):
+    return HttpResponse("ok", content_type="text/plain")
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("about/", views.about, name="about"),
+    path("admin/", admin.site.urls),
+    path("healthz", healthz),
+    path("", include("pages.urls")),
+    path("", include("events.urls")),
 ]
