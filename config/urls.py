e5bc2ce759ad5d7260e54e34146995a8a57d40e7
nadezhda8871s <nadezhda8871s@gmail.com>
2025-10-12 14:08:12 +0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -5,10 +5,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("events.urls")),
-    path("", include("pages.urls")),
-    path("i18n/", include("django.conf.urls.i18n")),
+    path("", include("events.urls")),                 # главные разделы
+    path("", include("pages.urls")),                  # /about/
+    path("i18n/", include("django.conf.urls.i18n")),  # /i18n/setlang/
 ]
 
-# Для небольшого проекта отдача media через Django допустима (Render).
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
