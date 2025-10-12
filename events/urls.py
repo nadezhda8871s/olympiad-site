ff28e3df9128d8dce07b98eedee2caee3521a203
nadezhda8871s <nadezhda8871s@gmail.com>
2025-10-12 14:40:03 +0300

    Update urls.py

--- events/urls.py
+++ events/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("olympiads", views.events_list_olymps, name="olympiads"),
-    path("contests", views.events_list_contests, name="contests"),
-    path("conferences", views.events_list_conferences, name="conferences"),
-    path("event/<slug:slug>", views.event_detail, name="event_detail"),
-    path("register/<slug:slug>", views.event_register, name="event_register"),
-    path("pay/<int:reg_id>", views.payment_mock, name="payment_mock"),
-    path("test/<int:reg_id>", views.test_view, name="test_view"),
-    path("api/search", views.search_api, name="search_api"),
-    path("admin/export/csv", views.export_csv_view, name="export_csv"),
+    path("olympiads/", views.events_list_olymps, name="olympiads"),
+    path("contests/", views.events_list_contests, name="contests"),
+    path("conferences/", views.events_list_conferences, name="conferences"),
+    path("event/<slug:slug>/", views.event_detail, name="event_detail"),
+    path("register/<slug:slug>/", views.event_register, name="event_register"),
+    path("pay/<int:reg_id>/", views.payment_mock, name="payment_mock"),
+    path("test/<int:reg_id>/", views.test_view, name="test_view"),
+    path("api/search", views.search_api, name="search_api"),  # без / по договорённости с JS
+    path("admin/export/csv/", views.export_csv_view, name="export_csv"),
 ]
