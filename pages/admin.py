023b41293ccc3057d9ffdfaf74a3c35749f4dc57
nadezhda8871s <nadezhda8871s@gmail.com>
2025-10-13 20:37:09 +0300

    Add files via upload

--- pages/admin.py
+++ pages/admin.py
@@ -3,7 +3,15 @@ from .models import AboutPage, SiteSettings
 
 @admin.register(AboutPage)
 class AboutPageAdmin(admin.ModelAdmin):
-    list_display = ("id", "updated_at")
+    list_display = ('id', 'updated_at')
+    fieldsets = (
+        (None, {
+            'fields': ('content_ru', 'content_en')
+        }),
+        ('Документы', {
+            'fields': ('privacy_doc', 'oferta_doc')
+        }),
+    )
 
 @admin.register(SiteSettings)
 class SiteSettingsAdmin(admin.ModelAdmin):
