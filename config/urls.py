d4c1765b1fa59258db7a4318ec5afa55a44268a0
nadezhda8871s <nadezhda8871s@gmail.com>
2025-10-13 09:23:33 +0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,23 +1,19 @@
 from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import path, include
-# Важно: правильный импорт set_language
 from django.views.i18n import set_language
 
 def healthz(_request):
     return HttpResponse("ok", content_type="text/plain")
 
 def favicon(_request):
-    # Пустая иконка, чтобы браузеры не сыпали 404
     return HttpResponse(b"", content_type="image/x-icon", status=200)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("healthz", healthz),
     path("favicon.ico", favicon),
-    # Для {% url 'set_language' %} из templates/base.html
     path("i18n/setlang/", set_language, name="set_language"),
-    # Ваши приложения
-    path("", include("pages.urls")),   # главная, о нас и пр.
-    path("", include("events.urls")),  # олимпиады/конкурсы/конференции
+    path("", include("pages.urls")),
+    path("", include("events.urls")),
 ]
