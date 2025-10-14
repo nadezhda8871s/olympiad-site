5d2f7efa5f16435e4c2c7e21cb5a02b2ccf57a57
nadezhda8871s <nadezhda8871s@gmail.com>
2025-10-14 15:08:40 +0300

    Add files via upload

--- pages/views.py
+++ pages/views.py
@@ -1,37 +1,19 @@
 from django.shortcuts import render
 from django.db.utils import OperationalError, ProgrammingError
-from django.apps import apps
 
 def home(request):
     featured = []
     try:
         from events.models import Event
-        featured = (
-            Event.objects
-            .filter(is_published=True, is_featured=True)
-            .order_by("sort_order", "-id")[:12]
-        )
-    except Exception:
-        featured = []
+        featured = Event.objects.filter(is_published=True, is_featured=True).order_by("sort_order", "-id")[:12]
+    except (OperationalError, ProgrammingError):
+        pass
     return render(request, "pages/home.html", {"featured_events": featured})
 
 def about(request):
-    page = None
-    Model = None
+    from .models import AboutPage
     try:
-        Model = apps.get_model("pages", "AboutPage")
-    except Exception:
-        Model = None
-    if Model:
-        try:
-            qs = Model.objects.all()
-            for flag in ("is_published", "published", "active", "is_active"):
-                if hasattr(Model, flag):
-                    qs = qs.filter(**{flag: True})
-                    break
-            if hasattr(Model, "updated_at"):
-                qs = qs.order_by("-updated_at")
-            page = qs.first()
-        except (OperationalError, ProgrammingError, Exception):
-            page = None
-    return render(request, "pages/about.html", {"page": page})
+        obj = AboutPage.objects.first()
+    except (OperationalError, ProgrammingError):
+        obj = None
+    return render(request, "pages/about.html", {"about": obj})
