b6cdc925a207a013eb3e607eb2e0d16c8c63893e
nadezhda8871s <nadezhda8871s@gmail.com>
2025-10-22 16:54:39 +0300

    Update urls.py

--- events/urls.py
+++ events/urls.py
@@ -8,10 +8,19 @@ urlpatterns = [
     path("conferences/", views.events_list_conferences, name="conferences"),
     path("event/<slug:slug>/", views.event_detail, name="event_detail"),
     path("register/<slug:slug>/", views.event_register, name="event_register"),
-    path("pay/<int:reg_id>/", views.payment_mock, name="payment_mock"),
+
+    # ЮKassa
+    path("pay/<int:reg_id>/", views.payment_start, name="payment_start"),
+    path("pay/success/", views.payment_success, name="payment_success"),
+    # webhook НЕ обязателен, можно не настраивать
+    path("pay/webhook/", views.payment_webhook, name="payment_webhook"),
+
+    # прочее
     path("test/<int:reg_id>/", views.test_view, name="test_view"),
     path("api/search", views.search_api, name="search_api"),
     path("admin/export/csv/", views.export_csv_view, name="export_csv"),
 ]
 
-urlpatterns += [path('event/<int:pk>/info-letter/', download_info_letter, name='event_info_letter')]
+urlpatterns += [
+    path('event/<int:pk>/info-letter/', download_info_letter, name='event_info_letter'),
+]
