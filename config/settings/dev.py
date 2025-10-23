73047b06fffdc35718300290ab2a0be5d74e1042
nadezhda8871s <nadezhda8871s@gmail.com>
2025-10-24 00:18:31 +0300

    Update dev.py

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -2,18 +2,9 @@ from .base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['*']
 
-# Development-specific settings
 SESSION_COOKIE_SECURE = False
 CSRF_COOKIE_SECURE = False
 
-# Use console email backend for development
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-# Disable caching in development
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
-    }
-}
