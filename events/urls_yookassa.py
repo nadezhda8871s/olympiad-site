42fca4b96b18e1e4dbf61093602b7961cbe6b044
nadezhda8871s <nadezhda8871s@gmail.com>
2025-10-24 13:16:19 +0300

    Update Yookassa URL patterns

--- events/urls_yookassa.py
+++ events/urls_yookassa.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from . import yookassa_views as views
 
-app_name = "events_yookassa"
-
 urlpatterns = [
-    path("notify/", views.yookassa_webhook, name="yookassa_webhook"),
-    path("start/<int:reg_id>/", views.start_payment_yookassa, name="start_payment_yookassa"),
+    path("success/", views.pay_success, name="yookassa_success"),
+    path("form/<int:reg_id>/", views.yookassa_payment_form, name="yookassa_payment_form"),
+    path("webhook/", views.yookassa_webhook, name="yookassa_webhook"),
 ]
