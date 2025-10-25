6a37d8998fc763682b8ecd1ff632ccf2254ae700
nadezhda8871s <nadezhda8871s@gmail.com>
2025-10-25 19:52:45 +0300

    Add URL for downloading info letter
    Added URL path for downloading the info letter.

--- events/urls.py
+++ events/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from . import views
+from . import views, views_infoletter
 
 app_name = 'events'
 
 urlpatterns = [
+    # Скачать информационное письмо (шаблоны зовут это именем 'event_info_letter')
+    path('<int:pk>/info-letter/', views_infoletter.download_info_letter, name='event_info_letter'),
+
     path('', views.event_list, name='list'),
     path('<int:pk>/', views.event_detail, name='detail'),
     path('<int:pk>/register/', views.event_register, name='register'),
